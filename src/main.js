260baf179651f102232cf637d404b6454b7e1c5d
TiennLevii <nguyentrachtien2401@gmail.com>
2024-01-06 14:06:04 +0700

    demo style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+import classes from "./main.module.css";
 const products = [
   { id: 1, name: "Product 1" },
   { id: 2, name: "Product 2" },
   { id: 3, name: "Product 3" },
 ];
 console.log(products);
+document.querySelector(
+  "#app"
+).innerHTML = `<div class="${classes.red}">Hello world</div>`;
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./components/*.{html,js}",
+  "./pages/*.{html,js}",
+  "./src/**/*.{html,js}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
